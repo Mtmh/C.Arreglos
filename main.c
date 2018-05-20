77b72009a84e06dab9adc100a8a729b5fcc794be
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-05-20 12:06:31 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int main(void)
+{
+   /* int a[10];// Estos es un arreglo 10 variables en una por ejemplo
+    a[0] = 5;
+    a[1] = 2;
+    a[2] = -23;
+    a[3] = 90;
+    a[4] = 45;
+    a[5] = 56;
+    a[6] = 7;
+    a[7] = 21;
+    a[8] = 0;
+    a[9] = -45;
+
+    printf("%d\n", a[0]);
+    printf("%d\n", a[1]);
+    printf("%d\n", a[2]);
+    printf("%d\n", a[3]);
+    printf("%d\n", a[4]);
+    printf("%d\n", a[5]);
+    printf("%d\n", a[6]);
+    printf("%d\n", a[7]);
+    printf("%d\n", a[8]);
+    printf("%d\n", a[9]);*/
+
+    /*int a[10] = { 5, 2, -23, 90, 45, 56, 7, 21, 0, -45};
+    printf("%d\n", a[0]);
+    printf("%d\n", a[1]);
+    printf("%d\n", a[2]);
+    printf("%d\n", a[3]);
+    printf("%d\n", a[4]);
+    printf("%d\n", a[5]);
+    printf("%d\n", a[6]);
+    printf("%d\n", a[7]);
+    printf("%d\n", a[8]);
+    printf("%d\n", a[9]);*/
+
+    /*int i, a[10];
+
+    for (i = 0; i < 10; ++i)
+        a[i] = i; //5;
+
+    printf("%d\n", a[0]);
+    printf("%d\n", a[1]);
+    printf("%d\n", a[2]);
+    printf("%d\n", a[3]);
+    printf("%d\n", a[4]);
+    printf("%d\n", a[5]);
+    printf("%d\n", a[6]);
+    printf("%d\n", a[7]);
+    printf("%d\n", a[8]);
+    printf("%d\n", a[9]);*/
+
+
+    int a[10] = { 3, 4, [9] = 7};
+
+    //a[9] = 7;
+
+    printf("%d\n", a[0]);
+    printf("%d\n", a[1]);
+    printf("%d\n", a[2]);
+    printf("%d\n", a[3]);
+    printf("%d\n", a[4]);
+    printf("%d\n", a[5]);
+    printf("%d\n", a[6]);
+    printf("%d\n", a[7]);
+    printf("%d\n", a[8]);
+    printf("%d\n", a[9]);
+
+
+
+    return 0;
+}
